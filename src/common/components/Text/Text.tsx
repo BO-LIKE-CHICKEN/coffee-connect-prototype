c8b9baaacde3a9e01700991d7469fe336559bff2
bosung baek <teabs1125@gmail.com>
2024-03-14 15:24:47 +0900

    refactor: 텍스트 컴포넌트들의 타입 변경 및 프롭스로 컬러를 받아올 수 있도록 개선

--- src/common/components/Text/Text.tsx
+++ src/common/components/Text/Text.tsx
@@ -1,9 +1,12 @@
 import styled from '@emotion/styled';
-import { ValueOf } from '@common/custom-types';
 import { theme } from '@styles/theme';
 
-const Text = styled.p<{ $color?: ValueOf<typeof theme> }>`
+const Text = styled.p<{ $color?: keyof typeof theme }>`
+  width: 100%;
+
   font-size: 18px;
+  font-weight: 500;
+  color: ${({ $color, theme }) => ($color ? theme[$color] : theme.black)};
 `;
 
 export default Text;
--- src/common/components/Title/Title.tsx
+++ src/common/components/Title/Title.tsx
@@ -1,11 +1,11 @@
 import styled from '@emotion/styled';
-import { ValueOf } from '@common/custom-types';
 import { theme } from '@styles/theme';
 
-const Title = styled.h1<{ $color?: ValueOf<typeof theme> }>`
+const Title = styled.h1<{ $color?: keyof typeof theme }>`
   width: 100%;
 
   font-size: 22px;
+  color: ${({ $color, theme }) => ($color ? theme[$color] : theme.black)};
   text-align: left;
 `;
 
